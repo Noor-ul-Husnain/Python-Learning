530a162d56d19f791e660c6115e12289a9a660ab
Noor ul Husnain <dev.noorulhusnain@gmail.com>
2024-02-17 22:26:05 +0500

    Calculater

---
+++ Calculater.py
@@ -0,0 +1,18 @@
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+
+sum_of_ab = a+b
+difference = a - b
+product = a * b
+division = a / b
+floor_division = a // b
+square_of_a = a ** 2
+square_of_b = b ** 2
+
+print("Sum: " + str(sum_of_ab))
+print("Difference: " + str(difference))
+print("Product: " + str(product))
+print("Division: " + str(division))
+print("Floor Division: " + str(floor_division))
+print("Square of 1sr number: " + str(square_of_a))
+print("Square of 2nd number: " + str(square_of_b))
